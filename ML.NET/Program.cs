3489f5c0179d98157bcf829804d0d381b48f09e6
calin.popescu <popescucalin99@gmail.com>
2020-12-03 20:55:10 +0200

    Add project files.

---
+++ ML.NET/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML;
+using System;
+using ML_NETML.Model;
+namespace ML.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput()
+            {
+                Category = @"Bar furniture",
+                Old_price = @"No old price",
+                Sellable_online = false,
+                Designer = @"Gucci",
+                Depth = 1F,
+                Height = 29F,
+                Width = 55F,
+            };
+
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine(result.Score);
+
+        }
+    }
+}
